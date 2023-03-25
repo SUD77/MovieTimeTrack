8c1f7178616a7d24c4b4daa2238f3c886b557641
shivangii-sri <shivangisrivastava762@gmail.com>
2023-03-25 22:50:55 +0530

    Adding data in User table.

---
+++ src/main/resources/db/migration/V3__inserting_data_into_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO movie.user (email,first_name,last_name,password,user_name)
+VALUES ('sriv@gmail.com','shivangi','srivastava','123','shiva90');
+
+INSERT INTO movie.user (email,first_name,last_name,password,user_name)
+VALUES ('hello@gmail.com','heena','hasty','1234','heyHeena');
